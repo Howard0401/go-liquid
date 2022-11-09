86c8eea105af5443ea71d4201b88e2e85d55c6c9
HowardChen <HowardChen@MB-HowardChen.local>
2022-11-09 11:54:00 +0800

    feature: 1. add go.mod 2. update order format

---
+++ go.mod
@@ -0,0 +1,14 @@
+module go-liquid
+
+go 1.19
+
+require (
+	github.com/BurntSushi/toml v1.2.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-numb/pusher-websocket-go v0.0.0-20190410135830-3f1b78f80a5b
+)
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
+)
--- order.go
+++ order.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"math"
+
+	"github.com/shopspring/decimal"
 )
 
 type Orders struct {
@@ -14,25 +16,42 @@ type Orders struct {
 }
 
 type Order struct {
-	ID                   int             `json:"id"`
-	OrderType            string          `json:"order_type"`
-	Quantity             json.Number     `json:"quantity"`
-	DiscQuantity         json.Number     `json:"disc_quantity"`
-	IcebergTotalQuantity string          `json:"iceberg_total_quantity"`
-	Side                 string          `json:"side"`
-	FilledQuantity       string          `json:"filled_quantity"`
-	Price                json.Number     `json:"price"`
-	CreatedAt            int64           `json:"created_at"`
-	UpdatedAt            int64           `json:"updated_at"`
-	Status               string          `json:"status"`
-	LeverageLevel        int             `json:"leverage_level"`
-	SourceExchange       string          `json:"source_exchange"`
-	ProductID            int             `json:"product_id"`
-	ProductCode          string          `json:"product_code"`
-	FundingCurrency      string          `json:"funding_currency"`
-	CurrencyPairCode     string          `json:"currency_pair_code"`
-	OrderFee             json.Number     `json:"order_fee"`
-	Executions           OrderExecutions `json:"executions"`
+	ID                        int64           `json:"id,omitempty"`
+	OrderType                 string          `json:"order_type,omitempty"`
+	Quantity                  decimal.Decimal `json:"quantity,omitempty"`
+	DiscQuantity              decimal.Decimal `json:"disc_quantity,omitempty"`
+	IcebergTotalQuantity      decimal.Decimal `json:"iceberg_total_quantity,omitempty"`
+	Side                      string          `json:"side,omitempty"`
+	FilledQuantity            decimal.Decimal `json:"filled_quantity,omitempty"`
+	Price                     decimal.Decimal `json:"price,omitempty"`
+	CreatedAt                 int             `json:"created_at,omitempty"`
+	UpdatedAt                 int             `json:"updated_at,omitempty"`
+	Status                    string          `json:"status,omitempty"`
+	LeverageLevel             int             `json:"leverage_level,omitempty"`
+	SourceExchange            interface{}     `json:"source_exchange,omitempty"`
+	ProductID                 int             `json:"product_id,omitempty"`
+	MarginType                interface{}     `json:"margin_type,omitempty"`
+	TakeProfit                interface{}     `json:"take_profit,omitempty"`
+	StopLoss                  interface{}     `json:"stop_loss,omitempty"`
+	TradingType               string          `json:"trading_type,omitempty"`
+	ProductCode               string          `json:"product_code,omitempty"`
+	FundingCurrency           string          `json:"funding_currency,omitempty"`
+	CryptoAccountID           interface{}     `json:"crypto_account_id,omitempty"`
+	CurrencyPairCode          string          `json:"currency_pair_code,omitempty"`
+	AveragePrice              decimal.Decimal `json:"average_price,omitempty"`
+	Target                    string          `json:"target,omitempty"`
+	OrderFee                  decimal.Decimal `json:"order_fee,omitempty"`
+	SourceAction              string          `json:"source_action,omitempty"`
+	UnwoundTradeID            interface{}     `json:"unwound_trade_id,omitempty"`
+	TradeID                   interface{}     `json:"trade_id,omitempty"`
+	ClientOrderID             string          `json:"client_order_id,omitempty"`
+	Settings                  interface{}     `json:"settings,omitempty"`
+	TrailingStopType          interface{}     `json:"trailing_stop_type,omitempty"`
+	TrailingStopValue         interface{}     `json:"trailing_stop_value,omitempty"`
+	StopTriggeredTime         interface{}     `json:"stop_triggered_time,omitempty"`
+	MarginUsed                decimal.Decimal `json:"margin_used,omitempty"`
+	MarginInterest            decimal.Decimal `json:"margin_interest,omitempty"`
+	UnwoundTradeLeverageLevel interface{}     `json:"unwound_trade_leverage_level,omitempty"`
 }
 
 type OrderExecutions []struct {
@@ -46,6 +65,22 @@ type OrderExecutions []struct {
 
 func (c *Client) GetOrder(orderID int) (Order, error) {
 	spath := fmt.Sprintf("/orders/%d", orderID)
+
+	var order Order
+	res, err := c.sendRequest("GET", spath, nil, nil)
+	if err != nil {
+		return order, err
+	}
+
+	if err := decode(res, &order); err != nil {
+		return order, err
+	}
+
+	return order, nil
+}
+
+func (c *Client) GetOrderByClientID(clientID string) (Order, error) {
+	spath := fmt.Sprintf("/orders/client:%s", clientID)
 
 	var order Order
 	res, err := c.sendRequest("GET", spath, nil, nil)
@@ -74,8 +109,8 @@ type OrdersFilter struct {
 	TradingType      string `json:"trading_type,omitempty"`
 }
 
-func (c *Client) GetOrders(filters OrdersFilter) (Orders, error) {
-	var orders Orders
+func (c *Client) GetOrders(filters OrdersFilter) (Order, error) {
+	var orders Order
 
 	j, err := json.Marshal(filters)
 	if err != nil {
